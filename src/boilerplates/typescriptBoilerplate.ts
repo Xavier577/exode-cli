14d89e78a12310da7bb24642c2a1482d0a7e8b7c
joseph <josephtsegen10@gmail.com>
2021-10-04 12:36:55 +0100

    removed some whitespaces

--- src/boilerplates/typescriptBoilerplate.ts
+++ src/boilerplates/typescriptBoilerplate.ts
@@ -1,15 +1,14 @@
 const PORT = process.env.PORT || 8080;
 const hostname = "127.0.0.1";
 
-export const indexTsFileBoilerplate = `
-import express from "express";
+export const indexTsFileBoilerplate = `import express from "express";
 const app = express()
 
 const PORT = ${PORT}; 
 const hostname = "${hostname}"
 
 app.get("/", (_req,res) => {
-             res.send("<h2>This project was created by excon</h2>")
+             res.send("<h2>Let's code!</h2>")
 })
 
 app.listen(PORT,hostname, () => console.log("listening on http://${hostname}:${PORT}"))
